12652395171f5e58499c566b18434a23b3140e11
Sathya Chilumula <sp.satyadeveloper@gmail.com>
2019-03-10 15:15:08 +0000

    Single-Variable and Multi-Variable using Python

---
+++ variables.py
@@ -0,0 +1,29 @@
+
+   # Variables using python
+   # Variables can be asign in two ways in python
+   # 1. Assigning Single variable using python
+   # 2. Assigning multi-variable using python
+
+# Assigning Single varibale
+a = 10 # a value is integer #
+name = 'DevOps' # string #
+salary = 600.75 # float #
+
+print (a)
+print (name)
+print (salary)
+
+#Assigning Multiple Variables
+
+company_name = "ATS Limited"
+company_employee_names = "RP, SP, MSR"
+company_employee_salary = "£250p/d, £300p/d, £350p/d"
+company_employee_names, company_employee_salary, company_id = [ "RP, SP, MSR", "£250p/d, £300p/d, £350p/d", "=5588999"]
+
+print (company_name)
+print (company_employee_names)
+print (company_employee_salary)
+print (company_employee_names,company_employee_salary,company_id)
+
+
+
